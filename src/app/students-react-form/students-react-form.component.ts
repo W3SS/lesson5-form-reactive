25e42c4e36a541c0745ec93e10a5a9d5a809ab1f
ภาณุสิทธิ์ เขื่อนคำ <panusit@gmail.com>
2017-11-09 16:25:28 +0700

    improve

--- src/app/students-react-form/students-react-form.component.ts
+++ src/app/students-react-form/students-react-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-students-react-form',
@@ -9,24 +9,73 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class StudentsReactFormComponent implements OnInit {
 
     registerForm: FormGroup;
+    user = {
+        secret: '',
+        userData: {
+            email: '',
+            fullName: '',
+            sex: '',
+            username: '',
+        },
+        hobbies: []
+    };
+    submitted = false;
 
     constructor() {
     }
 
     ngOnInit() {
         this.registerForm = new FormGroup({
+            'userData': new FormGroup({
+                'email': new FormControl(null, [
+                    Validators.required,
+                    Validators.email]),
+                'fullName': new FormControl(null, Validators.required),
+                'sex': new FormControl('Male'),
+                'username': new FormControl(null, Validators.required),
+            }),
             'secret': new FormControl('car'),
-            'email': new FormControl(null, [
-                Validators.required,
-                Validators.email]),
-            'fullName': new FormControl(null, Validators.required),
-            'sex': new FormControl('Male'),
-            'username': new FormControl(null),
+            'hobbies': new FormArray([])
         });
     }
 
-    onSubmit() {
-        console.log(this.registerForm);
+    onAddHobby() {
+        const control = new FormControl(null, Validators.required);
+        (<FormArray>this.registerForm.get('hobbies')).push(control);
     }
+
+    getHobbies() {
+        return (<FormArray>this.registerForm.get('hobbies')).controls;
+    }
+
+    onSubmit() {
+        this.submitted = true;
+
+        const userData = this.user.userData;
+        const registerForm = this.registerForm.value;
+
+        this.user.secret = registerForm.secret;
+        this.user.hobbies = registerForm.hobbies;
+        userData.email = registerForm.userData.email;
+        userData.fullName = registerForm.userData.fullName;
+        userData.sex = registerForm.userData.sex;
+        userData.username = registerForm.userData.username;
+
+        this.onReset();
+    }
+
+    onReset() {
+        this.registerForm.reset();
+        this.registerForm.patchValue({
+            userData: {
+                sex: 'Male'
+            },
+            secret: 'car'
+        });
+        const x = <FormArray>this.registerForm.controls.hobbies;
+        x.controls = [];
+    }
+
+
 
 }
